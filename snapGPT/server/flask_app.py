127b30e71d546beea019fabe8710a1be498bf0f2
BlockchainRelativity <blockchain.relativity@gmail.com>
2023-03-03 13:54:58 -0800

    refine: updates to comment out cohere

--- snapGPT/server/flask_app.py
+++ snapGPT/server/flask_app.py
@@ -17,7 +17,7 @@ src_dir = os.path.join(parent_dir, "src")
 sys.path.append(src_dir)
 
 # Import the services
-from services.cohereService import CohereService
+# from services.cohereService import CohereService
 from services.openaiService import OpenAIService
 
 # Flask app code...
@@ -38,7 +38,7 @@ def http_snap_resume():
     # openai_process.start()
     oa = OpenAIService(OPENAI_KEY)
     oa.connect()
-    corrected_resume = oa.resume_grammar_correct(resume_text)
+    corrected_resume = oa.resume_grammar_chatgpt(resume_text)
     return jsonify({"resume": corrected_resume}), 200
 
 
@@ -53,43 +53,43 @@ def http_embed_resume():
     return jsonify({"resume": embeded_resume}), 200
 
 
-@app.route("/colink/status/new", methods=["GET"])
-def http_show_new_colink_result():
-    data = request.get_json()
-    # @TODO pass/read colink_id from params instead of request body
-    colink_id = data.get("colink_id")
-    # If sequence is completed, response will exist in database
-    # else:
-    status_code = 404
-    result = {"result": f'your colink response with id:{colink_id} is not yet ready. Check again in 2-3 minutes.'}
-    return jsonify(result), status_code
+# @app.route("/colink/status/new", methods=["GET"])
+# def http_show_new_colink_result():
+#     data = request.get_json()
+#     # @TODO pass/read colink_id from params instead of request body
+#     colink_id = data.get("colink_id")
+#     # If sequence is completed, response will exist in database
+#     # else:
+#     status_code = 404
+#     result = {"result": f'your colink response with id:{colink_id} is not yet ready. Check again in 2-3 minutes.'}
+#     return jsonify(result), status_code
 
 
-@app.route("/colink/sequence", methods=["POST"])
-def http_colink_sequence():
-    # Get the request payload
-    data = request.get_json()
-    # Extract the required values from the payload
-    context_prompt = data.get("context_prompt")
-    user_prompt = data.get("user_prompt")
-    arr_prompts = [context_prompt, user_prompt]
-    seq = ast.literal_eval(data.get("seq"))
-    model_size = data.get("model_size")
-    # model_parameters =...
-    # print(f'context_prompt: {context_prompt}\nuser_prompt: {user_prompt}\nsequence:{seq}')
-    print("forking process")
-    colink_process = multiprocessing.Process(
-        target=cohere_start,
-        args=(COHERE_KEY,model_size, seq, arr_prompts)
-    )
-    colink_process.start()
-    # colink_process.join()
-    # @TODO: based on colinkSequence complexity, can we provide an estimate wait time for the
-    #   user check back if result is ready?
-    status_code = 200
-    result = { "colink_response": "We received your request. We're on it. Send a GET /colink/new?id=4 request in a few minutes to obtain results" }
-    # Return the result
-    return jsonify(result), status_code
+# @app.route("/colink/sequence", methods=["POST"])
+# def http_colink_sequence():
+#     # Get the request payload
+#     data = request.get_json()
+#     # Extract the required values from the payload
+#     context_prompt = data.get("context_prompt")
+#     user_prompt = data.get("user_prompt")
+#     arr_prompts = [context_prompt, user_prompt]
+#     seq = ast.literal_eval(data.get("seq"))
+#     model_size = data.get("model_size")
+#     # model_parameters =...
+#     # print(f'context_prompt: {context_prompt}\nuser_prompt: {user_prompt}\nsequence:{seq}')
+#     print("forking process")
+#     colink_process = multiprocessing.Process(
+#         target=cohere_start,
+#         args=(COHERE_KEY,model_size, seq, arr_prompts)
+#     )
+#     colink_process.start()
+#     # colink_process.join()
+#     # @TODO: based on colinkSequence complexity, can we provide an estimate wait time for the
+#     #   user check back if result is ready?
+#     status_code = 200
+#     result = { "colink_response": "We received your request. We're on it. Send a GET /colink/new?id=4 request in a few minutes to obtain results" }
+#     # Return the result
+#     return jsonify(result), status_code
 
 
 @app.route("/openai/test", methods=["GET"])
--- snapGPT/src/services/cohereService.py
+++ snapGPT/src/services/cohereService.py
@@ -1,94 +1,94 @@
-import cohere
-from multiprocess import join_active_children, gc_collect_exit, sleeep
+# import cohere
+# from multiprocess import join_active_children, gc_collect_exit, sleeep
 
-def cohere_start(api_key, model_size, seq, arr_prompts):
-    print("\n********** new process *************\n")
-    co = CohereService(api_key, model_size, seq, arr_prompts)
-    co.connect()
-    result = co.colinkSequence()
-    #@TODO: write to Database.
-    sleeep(1)
-    join_active_children()
-    gc_collect_exit(0)
+# def cohere_start(api_key, model_size, seq, arr_prompts):
+#     print("\n********** new process *************\n")
+#     co = CohereService(api_key, model_size, seq, arr_prompts)
+#     co.connect()
+#     result = co.colinkSequence()
+#     #@TODO: write to Database.
+#     sleeep(1)
+#     join_active_children()
+#     gc_collect_exit(0)
 
 
-class CohereService:
-    def __init__(self, api_key, model_size,seq, arr_prompts):
-        self.api_key = COHERE_API_KEY
-        self.model_size = model_size
-        self.seq = seq
-        self.prompts = arr_prompts
-        self.co = None
-        # self.co = connect()
-        # self.colinkSequence()
+# class CohereService:
+#     def __init__(self, api_key, model_size,seq, arr_prompts):
+#         self.api_key = COHERE_API_KEY
+#         self.model_size = model_size
+#         self.seq = seq
+#         self.prompts = arr_prompts
+#         self.co = None
+#         # self.co = connect()
+#         # self.colinkSequence()
 
-    def connect(self):
-        self.co = cohere.Client(self.api_key)
-        return self.co
+#     def connect(self):
+#         self.co = cohere.Client(self.api_key)
+#         return self.co
         
 
-    # links one request to another, or back to same one.
-    # setFlowSequence([('pc',1), ('pg',1000), ('pc',1)])
-    #   Classifies once +=> generates twice => classifies once
-    def colinkSequence(self):
-      # create file here, and open it.
-        transform_words = ""
-        for prompt in self.prompts:
-            transform_words+=prompt
-        for tupl in self.seq:
-            cycles = tupl[1]
-            # @TODO: can we do this with functional programming paradigm rather than a while loop?
-            while (cycles > 0):
-                print(f'\n****Cycle: {-1*(cycles - tupl[1])+1}***')
-                cycles -=1
-                if tupl[0] == 'pg':
-                    transform_words=self.__performGenerate(
-                        transform_words)
-                elif tupl[0] == 'pc':
-                    transform_words=self.__performClassify(
-                        transform_words,
-                        (context_prompt + user_prompt))
-                else:
-                    continue
-        return transform_words
+#     # links one request to another, or back to same one.
+#     # setFlowSequence([('pc',1), ('pg',1000), ('pc',1)])
+#     #   Classifies once +=> generates twice => classifies once
+#     def colinkSequence(self):
+#       # create file here, and open it.
+#         transform_words = ""
+#         for prompt in self.prompts:
+#             transform_words+=prompt
+#         for tupl in self.seq:
+#             cycles = tupl[1]
+#             # @TODO: can we do this with functional programming paradigm rather than a while loop?
+#             while (cycles > 0):
+#                 print(f'\n****Cycle: {-1*(cycles - tupl[1])+1}***')
+#                 cycles -=1
+#                 if tupl[0] == 'pg':
+#                     transform_words=self.__performGenerate(
+#                         transform_words)
+#                 elif tupl[0] == 'pc':
+#                     transform_words=self.__performClassify(
+#                         transform_words,
+#                         (context_prompt + user_prompt))
+#                 else:
+#                     continue
+#         return transform_words
 
-    def __performClassify(self,i,ex):
-        if not (self.co):
-            print('[Unauthorized] You must first connect to Cohere API.')
-            return 0
-        else:
-            response = self.co.classify(
-                model=self.model_size,
-                inputs=i,
-                examples=ex
-            )
-            print('Classification: {}'.format(response.classifications))
-            return response.classifications
-
-
-    def __performGenerate(self, p):
-        if not self.co:
-            print('[Unauthorized] You must first connect to Cohere API.')
-            return 0
-        else:
-            response = self.co.generate(
-                model=self.model_size, #'command-xlarge-nightly'
-                prompt=p,
-                max_tokens=75,
-                temperature=3.0,
-                k=0,
-                p=0.70,
-                frequency_penalty=0,
-                presence_penalty=0,
-                stop_sequences=[],
-                return_likelihoods='ALL')
-            print('GenTextElabo: {}'.format(response.generations[0].text))
-            return response.generations[0].text
+#     def __performClassify(self,i,ex):
+#         if not (self.co):
+#             print('[Unauthorized] You must first connect to Cohere API.')
+#             return 0
+#         else:
+#             response = self.co.classify(
+#                 model=self.model_size,
+#                 inputs=i,
+#                 examples=ex
+#             )
+#             print('Classification: {}'.format(response.classifications))
+#             return response.classifications
 
 
-  # @TODO: graphSequence
+#     def __performGenerate(self, p):
+#         if not self.co:
+#             print('[Unauthorized] You must first connect to Cohere API.')
+#             return 0
+#         else:
+#             response = self.co.generate(
+#                 model=self.model_size, #'command-xlarge-nightly'
+#                 prompt=p,
+#                 max_tokens=75,
+#                 temperature=3.0,
+#                 k=0,
+#                 p=0.70,
+#                 frequency_penalty=0,
+#                 presence_penalty=0,
+#                 stop_sequences=[],
+#                 return_likelihoods='ALL')
+#             print('GenTextElabo: {}'.format(response.generations[0].text))
+#             return response.generations[0].text
+
+
+#   # @TODO: graphSequence
       
-      # graphSequence
-    # def graphSequence(context_prompt, user_prompt, seq):    
+#       # graphSequence
+#     # def graphSequence(context_prompt, user_prompt, seq):    
 
 
